73fe9fe4c74acf883a9d4f4469eae4a80e21c329
siddharth733 <siddharthschauhan694@gmail.com>
2023-01-19 10:53:25 +0530

    commit

--- index.js
+++ index.js
@@ -27,18 +27,26 @@ app.post("/register", async (req, resp) => {
     } else {
       console.log(`connected as ${conn.threadId}`);
       const data = req.body;
-      conn.query("insert into users set ?", data, (err, result) => {
-        conn.release();
-        if (err) console.log(err);
-        else
-          jwt.sign({ data }, jwtKey, { expiresIn: "1h" }, (err, token) => {
-            if (err) {
-              resp.send("Something Went Wrong Please Try Again");
-            } else {
-              resp.send({ data, auth: token });
-            }
-          });
-      });
+      conn.query(
+        "insert into users (name,email,password) select ?, ?, ? where not exists (select 1 from users where email = ?)",
+        [req.body.name, req.body.email, req.body.password, req.body.email],
+        (err, result) => {
+          conn.release();
+          if (err) console.log(err);
+          else {
+            if (result.insertId === 0) {
+              resp.send({ err: "Email Exist" });
+            } else
+              jwt.sign({ data }, jwtKey, { expiresIn: "1h" }, (err, token) => {
+                if (err) {
+                  resp.send("Something Went Wrong Please Try Again");
+                } else {
+                  resp.send({ result, auth: token });
+                }
+              });
+          }
+        }
+      );
     }
   });
 });
@@ -59,11 +67,30 @@ app.post("/login", async (req, resp) => {
           jwt.sign({ data }, jwtKey, { expiresIn: "1h" }, (err, token) => {
             if (err) {
               resp.send("Something Went Wrong Please Try Again");
-              resp.send(err);
             } else {
               resp.send({ data, auth: token });
             }
           });
+        }
+      }
+    );
+  });
+});
+
+app.get("/searchData/:data", async (req, resp) => {
+  connection.getConnection((err, conn) => {
+    if (err) throw err;
+    console.log(`connected as ${conn.threadId}`);
+
+    const data = req.params.data;
+    conn.query(
+      "select * from users where name like ?",
+      [data + "%"],
+      (err, res) => {
+        conn.release();
+        if (err) throw err;
+        else {
+          resp.send(res);
         }
       }
     );
