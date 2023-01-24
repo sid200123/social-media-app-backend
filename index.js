c4686a9313b4fd82812ff7fa785f9d9f046f2201
siddharth733 <siddharthschauhan694@gmail.com>
2023-01-24 15:52:01 +0530

    done

--- index.js
+++ index.js
@@ -3,21 +3,39 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
 const jwtKey = "social-app";
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+var storage = multer.diskStorage({
+  destination: (req, file, callBack) => {
+    callBack(null, "images"); // './public/images/' directory name where save the file
+  },
+  filename: (req, file, callBack) => {
+    callBack(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+var upload = multer({
+  storage: storage,
+});
 
 const connection = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
   database: "Social_Media_App",
-  connectionLimit: 10,
+  connectionLimit: 30,
 });
 
 app.post("/register", async (req, resp) => {
@@ -58,7 +76,7 @@ app.post("/login", async (req, resp) => {
 
     const data = req.body;
     conn.query(
-      "select * from users where email =? and password =?",
+      "select * from users where email = ? and password = ?",
       [data.email, data.password],
       (err, res) => {
         conn.release();
@@ -68,12 +86,52 @@ app.post("/login", async (req, resp) => {
             if (err) {
               resp.send("Something Went Wrong Please Try Again");
             } else {
-              resp.send({ data, auth: token });
+              resp.send({ data: res, auth: token });
             }
           });
         }
       }
     );
+  });
+});
+
+app.put("/updateProfile", upload.single("profile"), async (req, resp) => {
+  connection.getConnection((err, conn) => {
+    if (err) throw err;
+    console.log(`connected as ${conn.threadId}`);
+
+    const data = req.body;
+    conn.query(
+      "update users set name = ?, address = ?, profile = ? where id = ?",
+      [data.name, data.address, data.profile, data.id],
+      (err, res) => {
+        conn.release();
+        if (err) throw err;
+        else {
+          if (err) {
+            resp.send("Something Went Wrong Please Try Again");
+          } else {
+            resp.send({ data: res });
+          }
+        }
+      }
+    );
+  });
+});
+
+app.get("/profileDetail/:id", async (req, resp) => {
+  connection.getConnection((err, conn) => {
+    if (err) throw err;
+    console.log(`connected as ${conn.threadId}`);
+
+    const id = req.params.id;
+    conn.query("select * from users where id = ?", id, (err, res) => {
+      conn.release();
+      if (err) throw err;
+      else {
+        resp.send(res);
+      }
+    });
   });
 });
 
