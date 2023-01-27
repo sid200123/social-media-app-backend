394c812721368c7e5b4a125006f3b65ec35776e3
siddharth733 <siddharthschauhan694@gmail.com>
2023-01-27 15:23:00 +0530

    commit

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ const port = process.env.PORT || 5000;
 const jwtKey = "social-app";
 
 app.use(cors());
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: false, limit: "5mb" }));
 
 var storage = multer.diskStorage({
-  destination: (req, file, callBack) => {
-    callBack(null, "./profile"); // './public/images/' directory name where save the file
-  },
+  destination: "profile",
   filename: (req, file, callBack) => {
     callBack(
       null,
@@ -28,7 +26,18 @@ var storage = multer.diskStorage({
 
 var upload = multer({
   storage: storage,
-});
+  fileFilter: (req, file, callBack) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      callBack(null, true);
+    } else {
+      callBack(null, false);
+      return callBack(new Error("Only Jpeg and Png format allowed"));
+    }
+  },
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+}).single("image");
 
 const connection = mysql.createPool({
   host: "localhost",
@@ -100,14 +109,24 @@ app.put("/updateProfile", async (req, resp) => {
     if (err) throw err;
     console.log(`connected as ${conn.threadId}`);
 
+    upload(req, resp, async (err) => {
+      if (err instanceof multer.MulterError) {
+        resp.send(err);
+      } else if (err) {
+        resp.send(err);
+      }
+      console.log(req.file);
+    });
+
     const data = req.body;
     conn.query(
       "update users set name = ?, address = ?, profile = ? where id = ?",
       [data.name, data.address, data.profile, data.id],
       (err, res) => {
         conn.release();
-        if (err) throw err;
-        else {
+        if (err) {
+          throw err;
+        } else {
           resp.send({ res });
         }
       }
