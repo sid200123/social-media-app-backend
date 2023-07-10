3f9be06b99798db704d39f57dbd74e504e69a2f7
siddharth733 <siddharthschauhan694@gmail.com>
2023-07-10 19:25:36 +0530

    final commit

--- Router/post.routes.js
+++ Router/post.routes.js
@@ -3,5 +3,6 @@ const postController = require("../component/post.controller");
 
 router.route("/addPost").post(postController.AddPost);
 router.route("/getPost").get(postController.GetPostData);
+router.route("/addLike").post(postController.LikeData);
 
 module.exports = router;
--- component/post.controller.js
+++ component/post.controller.js
@@ -27,6 +27,7 @@ const AddPost = async (req, resp) => {
       })
       .then((res) => {
         console.log(res);
+        console.log(req.body);
         pool
           .query("insert into posts set ?", [req.body])
           .then((err, result) => {
@@ -50,7 +51,41 @@ const GetPostData = async (req, resp) => {
   }
 };
 
+const LikeData = async (req, resp) => {
+  try {
+    const Data = {
+      dataId: [1, 2, 3, 4],
+    };
+
+    const jsonData = JSON.stringify(Data);
+
+    // pool.query(
+    //   "update posts set likes = ? where id='1'",
+    //   [jsonData],
+    //   (err, result) => {
+    //     if (err) throw err;
+    //     else {
+    //       resp.send(result);
+    //       console.log(result);
+    //     }
+    //   }
+    // );
+
+    pool.query("select likes from posts where id='1'", (err, result) => {
+      if (err) throw err;
+      const objData = JSON.parse(result[0].likes);
+      const checkAvaiblity = objData.dataId;
+      // objData.dataId.push(6);
+      console.log(checkAvaiblity.includes(3));
+      resp.send(checkAvaiblity);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 module.exports = {
   AddPost,
   GetPostData,
+  LikeData,
 };
